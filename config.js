07200f0a79ca0c9f11e4f3542ae84cab697396d4
Rainsho <djkyux@163.com>
2018-01-11 22:37:15 +0800

    init data

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    session_id: 'TQ2KU93/yUK+2HMM9ilwP6fC/JXSdetZnwWXtX97nq9W1UIT8n0lbwWmcixYmoAMIKlj/+KdZNaUaacjkQ9i562LJFYVL1SP/uDFAEfVioDVUk/XQx9D/UExLmTFtC9Xe+PCdtG8XLjk8dMgEIPa/g==',
+    score: 10,
+
+    // 游戏用常量
+    PER_ACTION: 8,
+    PER_REST: 15,
+}
+
+export { config };
