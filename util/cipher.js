217534bc0b32cc51d05c006f135343d1fdcba84c
Rainsho <djkyux@163.com>
2018-01-17 00:30:12 +0800

    Merge branch 'master' into dev

--- util/cipher.js
+++ util/cipher.js
@@ -8,16 +8,16 @@ const cipherEncoding = 'base64';
  * 目前 action_data 的加密方式为
  * 使用 aes-128-cbc 模式加密
  * 密码及偏移量同位 session_id 前 16 位
- * 
- * @param {string} data 
- * @param {string} key 
+ *
+ * @param {string} data
+ * @param {string} key
  */
 function encrypt(data, session_id) {
   const key = session_id.substr(0, 16);
   const chunks = [];
   const cipher = crypto.createCipheriv(algorithm, key, key);
   chunks.push(cipher.update(data, dataEncoding, cipherEncoding));
-  chunks.push(cipher.final(cipherEncoding))
+  chunks.push(cipher.final(cipherEncoding));
   return chunks.join('');
 }
 
